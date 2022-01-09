cde98a98b7ba5daa6a44fac84445c88f62752147
Tony Jin <nijynot@gmail.com>
2022-01-09 17:02:56 +0100

    feat: add initial registry contracts

--- src/libraries/Cast.sol
+++ src/libraries/Cast.sol
@@ -6,4 +6,9 @@ library Cast {
         require (x <= type(uint160).max, "CastOverflow");
         y = uint160(x);
     }
+
+    function u96(uint256 x) internal pure returns (uint96 y) {
+        require (x <= type(uint96).max, "CastOverflow");
+        y = uint96(x);
+    }
 }
